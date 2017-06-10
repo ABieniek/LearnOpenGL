7f05ba0968f137fb6e9b04ae1c50511ebc393110
Alex Bieniek <A.Bieniek789@gmail.com>
2017-06-09 21:26:59 -0500

    Finished Textures Lesson

---
+++ LearnOpenGL/Shaders/TexturesLesson.fs
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+in vec3 ourColor;
+in vec2 TexCoord;
+
+uniform sampler2D ourTexture0;
+uniform sampler2D ourTexture1;
+
+void main()
+{
+	FragColor = mix(texture(ourTexture0, TexCoord), 
+	texture(ourTexture1, TexCoord), .6);
+}
