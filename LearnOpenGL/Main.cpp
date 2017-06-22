ba556cdc24329215c3402e267a6d3dabf31f182e
Alex Bieniek <A.Bieniek789@gmail.com>
2017-06-21 20:54:17 -0500

    Finished Coordinate Systems Lesson, added multiple boxes, and put camera rotation controls on view matrix instead of model matrix

--- LearnOpenGL/Main.cpp
+++ LearnOpenGL/Main.cpp
@@ -108,11 +108,60 @@ void processInput(GLFWwindow* window)
 }
 
 float verticesBox[] = {
-	// positions          // colors           // texture coords
-	0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   2.0f, 2.0f,   // top right
-	0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   2.0f, 0.0f,   // bottom right
-	-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-	-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 2.0f    // top left 
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+
+glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
 };
 
 unsigned int indicesBox[] = {
@@ -152,27 +201,20 @@ int main()
 	Shader shaders("Shaders\\CoordinateSystemsLesson.vs",
 		"Shaders\\CoordinateSystemsLesson.fs");
 
-	unsigned int VBOBox, VAOBox, EBOBox;
+	unsigned int VBOBox, VAOBox;
 	glGenVertexArrays(1, &VAOBox);
 	glGenBuffers(1, &VBOBox);
-	glGenBuffers(1, &EBOBox);
 	glBindVertexArray(VAOBox);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBOBox);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verticesBox), verticesBox, GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOBox);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicesBox), indicesBox, GL_STATIC_DRAW);
-
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-	//color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
 	// texture coord attribute
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
 	/*	TEXTURE STUFF	*/
 	stbi_set_flip_vertically_on_load(true);
@@ -197,81 +239,58 @@ int main()
 		std::cout << "Failed to load texture" << std::endl;
 	stbi_image_free(dataBox);
 
-	// face
-	unsigned int textureFace;
-	glGenTextures(1, &textureFace);
-	glBindTexture(GL_TEXTURE_2D, textureFace);
-	//set texture wrapping and filtering options of textureFace
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	// load image, creat texture, generate mipmaps
-	int widthFace, heightFace, nrChannelsFace;
-	unsigned char *dataFace = stbi_load("Textures\\awesomeface.png", &widthFace, &heightFace, &nrChannelsFace, 0);
-	if (dataFace)
-	{
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthFace, heightFace, 0, GL_RGBA, GL_UNSIGNED_BYTE, dataFace);
-		glGenerateMipmap(GL_TEXTURE_2D);
-	}
-	else
-		std::cout << "Failed to load texture" << std::endl;
-	stbi_image_free(dataFace);
-
 	shaders.use();
 	shaders.setInt("ourTexture0", 0);
-	shaders.setInt("ourTexture1", 1);
 
 	// transformation stuff
 	glm::mat4 trans;
 	trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));
 	trans = glm::rotate(trans, glm::degrees(90.0f), glm::vec3(0.0, 0.0, 1.0));
 
-
 	unsigned int transformLoc = glGetUniformLocation(shaders.program, "transform");
 	glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+	glEnable(GL_DEPTH_TEST);
 
 	// rendering loop
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
 
-		// changing the mix percentage through user input
-		shaders.setFloat("mixPercent", .2);
-
 		// setting up clear screen stuff
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		//bind texture
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, textureBox);
-		glActiveTexture(GL_TEXTURE1);
-		glBindTexture(GL_TEXTURE_2D, textureFace);
-
+		glBindVertexArray(VAOBox);
 		// set up transformations
 		glm::mat4 model;
 		glm::mat4 view;
 		glm::mat4 projection;
-		model = glm::rotate(model, glm::radians((float) yRot), glm::vec3(1.0f, 0.0f, 0.0f)); // the final argument determines axis of rotation, somehow
-		model = glm::rotate(model, glm::radians((float) xRot), glm::vec3(0.0f, 0.0f, 1.0f));
-		model = glm::rotate(model, glm::radians((float) zRot), glm::vec3(0.0f, 1.0f, 0.0f));
-		// retrieve matrix uniform locations
-		view = glm::translate(view, glm::vec3(xPos, yPos, zPos));
-		projection = glm::perspective(glm::radians((float) -55.0), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
-		
-		shaders.setMat4("model", model);
-		shaders.setMat4("view", view);
-		shaders.setMat4("projection", projection);
 
-		glBindVertexArray(VAOBox);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			model = glm::translate(model, cubePositions[i]);
+			// retrieve matrix uniform locations
+			view = glm::translate(view, glm::vec3(xPos, yPos, zPos));
+			view = glm::rotate(view, glm::radians((float)yRot), glm::vec3(1.0f, 0.0f, 0.0f)); // the final argument determines axis of rotation, somehow
+			view = glm::rotate(view, glm::radians((float)xRot), glm::vec3(0.0f, 0.0f, 1.0f)); // This rotation seems wrong, I'm misunderstanding something
+			view = glm::rotate(view, glm::radians((float)zRot), glm::vec3(0.0f, 1.0f, 0.0f));
+			projection = glm::perspective(glm::radians((float)-55.0), (float)SCREEN_WIDTH / (float)SCREEN_HEIGHT, 0.1f, 100.0f);
+
+			shaders.setMat4("model", model);
+			shaders.setMat4("view", view);
+			shaders.setMat4("projection", projection);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 	glDeleteVertexArrays(1, &VAOBox);
 	glDeleteBuffers(1, &VBOBox);
-	glDeleteBuffers(1, &EBOBox);
 
 	glfwTerminate();
 	return 0;
